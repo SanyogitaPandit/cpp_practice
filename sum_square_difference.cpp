855605171c07d51968e75089ca4e371c110f74f0
Sanyogita <sanyogitapandit1989@gmail.com>
2020-09-17 20:53:43 +0000

    rename file

---
+++ sum_square_difference.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+long sum_square_difference(long n)
+{
+  long diff = 0;
+  int l, k; 
+  
+    // Sum of the squares of the 
+    // first n natural numbers is 
+    l = (n * (n + 1) * (2 * n + 1)) / 6; 
+      
+    // Sum of n naturals numbers 
+    k = (n * (n + 1)) / 2; 
+  
+    // Square of k 
+    k = k * k; 
+      
+    // Differences between l and k 
+    diff = abs(l - k);    
+
+  return diff;
+}
