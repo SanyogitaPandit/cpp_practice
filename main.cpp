339fddcef605eb72a4d739f54303855bf6fe7128
Sanyogita <sanyogitapandit1989@gmail.com>
2020-09-18 02:02:16 +0000

    max product

--- main.cpp
+++ main.cpp
@@ -1,13 +1,21 @@
 #include <iostream>
-#include <vector>
+
 using namespace std;
 
 int main()
 { 
-  long long n = -1;
+  /*long long n = -1;
   while(n != 0){
     cout<<"\n" << "Enter a number: ";  
     cin >> n ;
     cout<<"\n" << (n);
+  }*/
+  std::string no;
+  int cnt;
+  while(no[0] != 'q'){
+    cout<<"\n" << "Enter a number and digit count: ";  
+    cin >> no ;
+    cin >> cnt ;
+    cout<<"\n";
   }
 }
