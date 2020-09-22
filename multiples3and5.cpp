dbd347b84ca1f882f07259445509c4b1d8692b3a
Sanyogita <48774009+SanyogitaPandit@users.noreply.github.com>
2020-09-21 21:56:42 -0400

    Update multiples3and5.cpp

--- multiples3and5.cpp
+++ multiples3and5.cpp
@@ -1,16 +1,40 @@
-int multiple3n5(int n)
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include<stdio.h>
+#include <math.h>
+using namespace std;
+
+long long multiple3n5(int n)
 {
-  if(n < 3)
-    return 0;
-  
-  int sum = 3;
-  for(int i = 4; i<n; ++i)
+  if((n < 3) || (n > pow(10, 9)))
   {
-    if( i % 3 == 0 || i % 5 == 0)
-    {
-      sum += i;
-    }    
-  }
+    return 0;
+  }  
+    
+  long long sum = 0;
+  long x = (n-1) / 3;
+  sum = 3*(x*(x + 1)/2);
+  x = (n-1) / 5;
+  sum += 5*(x*(x + 1)/2);
+  x = (n-1) / 15;
+  sum -= 15*(x*(x + 1)/2);
 
   return sum;
+}
+
+int main(){
+    int t;
+    cin >> t;
+    if(t<=pow(10, 5))
+    {        
+    for(int a0 = 0; a0 < t; a0++){
+        int n;
+        cin >> n;                
+        cout<< multiple3n5(n) << "\n";
+    }
+    }
+    return 0;
 }
